4c3e1d0f9407b838c011132c20b0ce37f72ea1d3
Riceball LEE <snowyu.lee@gmail.com>
2025-12-02 10:57:11 +0800

    fix(test): only essential test when build

--- test/utils/scripts/downloadAllTestModels.ts
+++ test/utils/scripts/downloadAllTestModels.ts
@@ -1,4 +1,18 @@
-import {downloadAllModels} from "../modelFiles.js";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { downloadAllModels, modelGroups } from "../modelFiles.js";
 
-await downloadAllModels();
+const argv = await yargs(hideBin(process.argv))
+  .option("group", {
+    type: "string",
+    array: true,
+    choices: Object.keys(modelGroups),
+    description: "Model groups to download"
+  })
+  .help()
+  .parse();
+
+await downloadAllModels({
+  groups: argv.group as any
+});
 process.exit(0);
