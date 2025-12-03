10578984ff506919efc24185d57fa881ae2e15cb
Riceball LEE <snowyu.lee@gmail.com>
2025-12-03 15:16:01 +0800

    fix(build): the package name

--- scripts/rerunSemanticReleaseGithubSuccess.ts
+++ scripts/rerunSemanticReleaseGithubSuccess.ts
@@ -1,8 +1,8 @@
 import path from "path";
-import {fileURLToPath} from "url";
+import { fileURLToPath } from "url";
 import fs from "fs-extra";
 // @ts-ignore
-import {verifyConditions as githubVerifyConditions, success as githubSuccess} from "@semantic-release/github";
+import { verifyConditions as githubVerifyConditions, success as githubSuccess } from "@semantic-release/github";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -14,7 +14,7 @@ const githubPluginConfig = {};
 const context = {
     ...brokenReleaseDryRunResult,
     options: {
-        repositoryUrl: "https://github.com/withcatai/node-llama-cpp.git"
+        repositoryUrl: "https://github.com/isdk/llama-node.git"
     },
     logger: console,
     env: process.env
@@ -24,12 +24,12 @@ for (const release of context.releases) {
     if (release.pluginName === "@semantic-release/npm") {
         if (release.url == null) {
             release.name = "npm package (@latest dist-tag)";
-            release.url = "https://www.npmjs.com/package/node-llama-cpp/v/" + release.version;
+            release.url = "https://www.npmjs.com/package/@isdk/llama-node/v/" + release.version;
         }
     } else if (release.pluginName === "@semantic-release/github") {
         if (release.url == null) {
             release.name = "GitHub release";
-            release.url = "https://github.com/withcatai/node-llama-cpp/releases/tag/" + release.gitTag;
+            release.url = "https://github.com/isdk/llama-node/releases/tag/" + release.gitTag;
         }
     }
 }
