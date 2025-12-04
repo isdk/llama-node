1e084734cc1a1f9a75f8ab42c06a1d133d13446f
Riceball LEE <snowyu.lee@gmail.com>
2025-12-04 18:13:42 +0800

    fix: minor changes

--- scripts/prepareCreateNodeLlamaCppModuleForPublish.ts
+++ scripts/prepareCreateNodeLlamaCppModuleForPublish.ts
@@ -1,11 +1,11 @@
 import path from "path";
-import {fileURLToPath} from "url";
+import { fileURLToPath } from "url";
 import yargs from "yargs";
-import {hideBin} from "yargs/helpers";
+import { hideBin } from "yargs/helpers";
 import fs from "fs-extra";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const createPackageModulePackageJsonPath = path.join(__dirname, "..", "packages", "create-node-llama-cpp", "package.json");
+const createPackageModulePackageJsonPath = path.join(__dirname, "..", "packages", "create-llama-node", "package.json");
 
 const argv = await yargs(hideBin(process.argv))
     .option("packageVersion", {
@@ -14,14 +14,14 @@ const argv = await yargs(hideBin(process.argv))
     })
     .argv;
 
-const {packageVersion} = argv;
+const { packageVersion } = argv;
 if (packageVersion === "")
     throw new Error("packageVersion is empty");
 
 const packageJson = await fs.readJson(createPackageModulePackageJsonPath);
 packageJson.version = packageVersion;
-packageJson.dependencies["node-llama-cpp"] = packageVersion;
+packageJson.dependencies["@isdk/llama-node"] = packageVersion;
 delete packageJson.devDependencies;
 
-await fs.writeJson(createPackageModulePackageJsonPath, packageJson, {spaces: 2});
-console.info(`Updated "create-node-llama-cpp/package.json" to version "${packageVersion}"`);
+await fs.writeJson(createPackageModulePackageJsonPath, packageJson, { spaces: 2 });
+console.info(`Updated "create-llama-node/package.json" to version "${packageVersion}"`);
